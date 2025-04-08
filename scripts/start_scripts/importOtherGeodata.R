ab766f9a313ea5ce818e70162cfc4a1b059434d3
Umberto Vesco <umberto.vesco@gmail.com>
2025-04-08 21:48:38 +0200

    cambio geodati limiti amministrativi

--- scripts/start_scripts/importOtherGeodata.R
+++ scripts/start_scripts/importOtherGeodata.R
@@ -1,4 +1,4 @@
 library(sf)
 
-comuni <- st_read(file.path(parametri$pubdata_cache_dir, "Ambiti_Amministrativi-Comuni.shp"),
+comuni <- st_read(file.path(parametri$pubdata_cache_dir, "Ambiti_Amministrativi-Comuni_2024.shp"),
                   quiet = TRUE)
